d5885ca8f197f21bf1863d0a8f38ed20e37a48e1
AlanCienega <craulalan115@gmail.com>
2022-11-23 22:30:24 -0600

    actualización de  dependencias

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
-var app = require('express')(),
-http = require('http').createServer(app),
-io = require('socket.io')(http),
-port = process.env.PORT || 3000,
-publicDir = `${__dirname}/public`
-http.listen(port, () =>{
-    console.log('################################################')
-    console.log('#       Servidor iniciado en localhost:%d    #', port);
-    console.log('################################################')
-    console.log('\n')
-    console.log('####### ######## ##     ## ######## ######  ########')
-    console.log('#       #      # # #   # # #      # #     # #      #')
-    console.log('#       ######## #  # #  # ######## ######  ######## ')
-    console.log('#       #      # #   #   # #      # #   #   #      #')
-    console.log('####### #      # #       # #      # #     # #      #')
-    console.log('\n')
-    console.log('###################################################')
-    
-})
+var app = require("express")(),
+  http = require("http").createServer(app),
+  io = require("socket.io")(http),
+  port = process.env.PORT || 3000,
+  publicDir = `${__dirname}/public`;
+http.listen(port, () => {
+  console.log("################################################");
+  console.log("#       Servidor iniciado en localhost:%d    #", port);
+  console.log("################################################");
+  console.log("\n");
+  console.log("####### ######## ##     ## ######## ######  ########");
+  console.log("#       #      # # #   # # #      # #     # #      #");
+  console.log("#       ######## #  # #  # ######## ######  ######## ");
+  console.log("#       #      # #   #   # #      # #   #   #      #");
+  console.log("####### #      # #       # #      # #     # #      #");
+  console.log("\n");
+  console.log("###################################################");
+});
 
 app
-    .get('/', (req, res) => {
-        res.sendFile(`${publicDir}/client.html`)
-    })
-    .get('/streaming', (req, res) =>{
-        res.sendFile(`${publicDir}/server.html`)
-    })
+  .get("/", (req, res) => {
+    res.sendFile(`${publicDir}/client.html`);
+  })
+  .get("/streaming", (req, res) => {
+    res.sendFile(`${publicDir}/server.html`);
+  });
 
-    io.on('connection', (socket) =>{
-        socket.on('streaming', (image) => {
-            io.emit('playStream', image)
-        })
-    })
+io.on("connection", (socket) => {
+  socket.on("streaming", (image) => {
+    io.emit("playStream", image);
+  });
+});
